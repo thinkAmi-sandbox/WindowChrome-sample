14348a2e779aea65336fac04729c7200ab7b9453
thinkAmi <dev.thinkami+git@gmail.com>
2014-12-04 05:47:24 +0900

    first commit

---
+++ WindowChrome-sample/MainViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;                           // Console
+using System.Windows;                   // Window, SystemCommands
+using Microsoft.TeamFoundation.MVVM;    // ViewModelBase
+
+namespace WindowChrome_sample
+{
+    public partial class MainViewModel : ViewModelBase
+    {
+        // WindowStateへのデータバインディング
+        private WindowState _windowState;
+        public WindowState WindowState
+        {
+            get { return _windowState; }
+            set
+            {
+                _windowState = value;
+                RaisePropertyChanged("WindowState");
+
+                // タイトルバーの挙動を確認するためのコンソール出力
+                Console.WriteLine("WindowState:" + _windowState.ToString());
+            }
+        }
+
+
+        // 閉じるボタン
+        private void ExecuteCloseCommand(object x)
+        {
+            if (x != null)
+            {
+                var window = (Window)x;
+
+                // SystemCommandsとCloseメソッド、どちらを使っても閉じることができる
+                SystemCommands.CloseWindow(window);
+                //window.Close();
+            }
+        }
+
+
+        // 最大化ボタン
+        private void ExecuteMaximizeCommand(object x)
+        {
+            if (x != null)
+            {
+                var window = (Window)x;
+                SystemCommands.MaximizeWindow(window);
+            }
+        }
+
+        private bool CanExecuteMaximizeCommand(object x)
+        {
+            if (x == null) return false;
+
+            var window = (Window)x;
+            return window.WindowState == WindowState.Maximized ? false : true;
+        }
+
+
+        // 最小化ボタン
+        private void ExecuteMinimizeCommand(object x)
+        {
+            if (x != null)
+            {
+                var window = (Window)x;
+                SystemCommands.MinimizeWindow(window);
+            }
+        }
+
+
+        // 復元ボタン
+        private void ExecuteRestoreCommand(object x)
+        {
+            if (x != null)
+            {
+                var window = (Window)x;
+                SystemCommands.RestoreWindow(window);
+            }
+        }
+
+        private bool CanExecuteRestoreCommand(object x)
+        {
+            if (x == null) return false;
+
+            var window = (Window)x;
+
+            // タイトルバーの挙動を確認するためのコンソール出力
+            Console.WriteLine("CanRestore:" + window.WindowState.ToString());
+
+            return window.WindowState == WindowState.Normal ? false : true;
+        }
+    }
+}
---
+++ WindowChrome-sample/MainViewModelT4.cs
@@ -0,0 +1,65 @@
+﻿
+using System.Windows.Input;				// ICommand
+using Microsoft.TeamFoundation.MVVM;    // ViewModelBase
+
+namespace WindowChrome_sample
+{
+    public partial class MainViewModel : ViewModelBase
+    {
+
+
+
+
+
+        private ICommand _MinimizeCommand;
+        public ICommand MinimizeCommand
+        {
+            get
+            {
+                if (_MinimizeCommand == null)
+                {
+                    _MinimizeCommand = new RelayCommand(ExecuteMinimizeCommand);
+                }
+                return _MinimizeCommand;
+            }
+        }
+        private ICommand _CloseCommand;
+        public ICommand CloseCommand
+        {
+            get
+            {
+                if (_CloseCommand == null)
+                {
+                    _CloseCommand = new RelayCommand(ExecuteCloseCommand);
+                }
+                return _CloseCommand;
+            }
+        }
+
+        private ICommand _MaximizeCommand;
+        public ICommand MaximizeCommand
+        {
+            get
+            {
+                if (_MaximizeCommand == null)
+                {
+                    _MaximizeCommand = new RelayCommand(ExecuteMaximizeCommand, CanExecuteMaximizeCommand);
+                }
+                return _MaximizeCommand;
+            }
+        }
+        private ICommand _RestoreCommand;
+        public ICommand RestoreCommand
+        {
+            get
+            {
+                if (_RestoreCommand == null)
+                {
+                    _RestoreCommand = new RelayCommand(ExecuteRestoreCommand, CanExecuteRestoreCommand);
+                }
+                return _RestoreCommand;
+            }
+        }
+
+    }
+}
